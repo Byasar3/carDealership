820791ec2bed9d84b9a92b003c69e8eda3f44d9a
Berna Yasar <bernayasar@hotmail.co.uk>
2023-01-11 18:26:33 +0000

    few more car tests

--- car.test.js
+++ car.test.js
@@ -1,11 +1,28 @@
 const Car = require('./car');
 
+let miniCooper;
+
+beforeEach( () => {
+     miniCooper = new Car ("BMW", 22000, "3-cylinder")
+})
+
 describe ('testing getters', () => {
 
     test('can get manufacturer', () => {
-        const miniCooper = new Car ("BMW", 22000, "3-cylinder")
         actual = miniCooper.getManufacturer();
         expected = "BMW";
+        expect(actual).toBe(expected);
+    })
+
+    test('can get price)', () => {
+        actual = miniCooper.getPrice();
+        expected = 22000;
+        expect(actual).toBe(expected);
+    })
+
+    test('can get engine', () => {
+        actual = miniCooper.getEngine();
+        expected = "3-cylinder";
         expect(actual).toBe(expected);
     })
 })
